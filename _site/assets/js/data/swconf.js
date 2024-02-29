64d8dcf26f1265e165cb79d1205004bd5db2e868
NHN <nhn@Taeok-MackBook2.local>
2024-02-29 17:44:01 +0900

    update

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,34 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1709196203',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],allowHosts: [
+      
+        'chirpy-img.netlify.app',
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'polyfill.io'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
